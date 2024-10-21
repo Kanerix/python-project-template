356eedc6c197d0039ba17c3cb6fde476ae358644
Kasper <dkkasjoe@hotmail.com>
2024-10-22 01:02:07 +0200

    Wrote example unit test

--- tests/main_test.py
+++ tests/main_test.py
@@ -7,5 +7,7 @@ from package.main import hello_polars
 
 
 def test_hello_ploars():
-    """Tests hello_polars function."""
+    """Tests the hello_polars() function."""
     df = hello_polars("Kasper", 21)
+    assert df["name"][0] == "Kasper"
+    assert df["age"][0] == 21
